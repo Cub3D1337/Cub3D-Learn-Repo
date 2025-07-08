215cf7928a23d2219fb17ef1b4897362085b13d9
Abdellah-Nsila <dawndawn074@gmail.com>
2025-07-08 14:57:18 +0100

    Add build 3D map utils

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 20:06:22 by abnsila           #+#    #+#             */
-/*   Updated: 2025/07/07 20:53:59 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/07/08 14:55:21 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,45 @@ t_bool	touch(t_cub *cub, double x, double y)
 	return (false);
 }
 
-void	draw_ray(t_cub *cub, double start_x, int i)
+double	fixed_dist(t_cub *cub, double x1, double y1, double x2, double y2)
+{
+	double	delta_x;
+	double	delta_y;
+	double	angle;
+	double	fixed_dist;
+
+	delta_x = x2 - x1;
+	delta_y = y2 - y1;
+	angle = atan2(delta_y, delta_x) - cub->ply.angle;
+	fixed_dist = sqrt(pow(delta_x, 2) + pow(delta_y, 2)) * cos(angle);
+	return (fixed_dist);
+}
+
+void	draw_line(t_cub *cub, double ray_x, double ray_y, int i)
+{
+	// Draw lines
+	double	dist;
+	double	height;
+	int		start_y;
+	int		end;
+	int		screen_x;
+
+	dist = fixed_dist(cub, cub->ply.x ,cub->ply.y ,ray_x, ray_y);
+	height = (MAP_SIZE / dist) * (cub->width / 2);
+	start_y = (cub->height - height) / 2;
+	end = start_y + height;
+
+	// 👇 Move 3D column to the right side of window
+	// screen_x = MAP_VIEW_WIDTH + i;
+	screen_x = i;
+	while (start_y < end)
+	{
+		put_pixel(cub, screen_x, start_y, 0x0000ff);
+		start_y++;
+	}
+}
+
+void	draw_ray(t_cub *cub, double ray_angle, int i, t_bool build)
 {
 	(void)i;
 	double	ray_x;
@@ -45,14 +83,16 @@ void	draw_ray(t_cub *cub, double start_x, int i)
 	ray_x = cub->ply.x + cub->ply.size / 2.0;
 	ray_y = cub->ply.y + cub->ply.size / 2.0;
 	// Get current angle cos and sin
-	cos_angle = cos(start_x);
-	sin_angle = sin(start_x);
+	cos_angle = cos(ray_angle);
+	sin_angle = sin(ray_angle);
 	while (!touch(cub, ray_x, ray_y))
 	{
 		put_pixel(cub, ray_x, ray_y, 0x00ff00);
-		ray_x += cos_angle;
-		ray_y += sin_angle;
+		ray_x += cos_angle * 0.1;
+		ray_y += sin_angle * 0.1;
 	}
+	if (build)
+		draw_line(cub, ray_x, ray_y, i);
 }
 
 void	draw_square(t_cub *cub, int x, int y, int size, int color)
