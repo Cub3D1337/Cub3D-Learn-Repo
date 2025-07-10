8ba22852fc732d9c803d66f30101f62e59bb7369
Abdellah-Nsila <dawndawn074@gmail.com>
2025-07-10 20:13:44 +0100

    new dda logic

--- srcs/dda.c
+++ srcs/dda.c
@@ -6,81 +6,89 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 15:46:22 by abnsila           #+#    #+#             */
-/*   Updated: 2025/07/09 18:18:12 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/07/10 20:11:55 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-t_pointF dda(t_cub *cub, t_pointF start, double rayDirX, double rayDirY, double *out_dist)
+static void	setup_step(t_dda *dda, t_pointF ray_dir)
 {
-	t_pointI map;
-	t_pointF sideDist;
-	t_pointF deltaDist;
-	t_pointI step;
-	t_bool hit = false;
-	int side; // 0 = x side, 1 = y side
-
-	map.x = (int)(start.x / MAP_SIZE);
-	map.y = (int)(start.y / MAP_SIZE);
-
-	deltaDist.x = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-	deltaDist.y = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
-
-	if (rayDirX < 0)
-	{
-		step.x = -1;
-		sideDist.x = (start.x / MAP_SIZE - map.x) * deltaDist.x;
-	}
+	if (ray_dir.x < 0)
+		dda->step.x = -1;
 	else
-	{
-		step.x = 1;
-		sideDist.x = (map.x + 1.0 - start.x / MAP_SIZE) * deltaDist.x;
-	}
-	if (rayDirY < 0)
-	{
-		step.y = -1;
-		sideDist.y = (start.y / MAP_SIZE - map.y) * deltaDist.y;
-	}
+		dda->step.x = 1;
+	if (ray_dir.y < 0)
+		dda->step.y = -1;
 	else
-	{
-		step.y = 1;
-		sideDist.y = (map.y + 1.0 - start.y / MAP_SIZE) * deltaDist.y;
-	}
+		dda->step.y = 1;
+}
 
-	while (!hit)
+static void	setup_dda(t_dda *dda, t_pointF ply_pos, t_pointF ray_dir)
+{
+	dda->map.x = (int)(ply_pos.x / MAP_SIZE);
+	dda->map.y = (int)(ply_pos.y / MAP_SIZE);
+	dda->unit.x = fabs(1 / ray_dir.x);
+	dda->unit.y = fabs(1 / ray_dir.y);
+	dda->hit = false;
+	dda->max_steps = fmax(MAP_WIDTH, MAP_HEIGHT);
+	setup_step(dda, ray_dir);
+	if (ray_dir.x < 0)
+		dda->hypotenuse_dist.x = ((ply_pos.x / MAP_SIZE) - dda->map.x)
+			* dda->unit.x;
+	else
+		dda->hypotenuse_dist.x = (dda->map.x + 1.0 - (ply_pos.x / MAP_SIZE))
+			* dda->unit.x;
+	if (ray_dir.y < 0)
+		dda->hypotenuse_dist.y = ((ply_pos.y / MAP_SIZE) - dda->map.y)
+			* dda->unit.y;
+	else
+		dda->hypotenuse_dist.y = (dda->map.y + 1 - (ply_pos.y / MAP_SIZE))
+			* dda->unit.y;
+}
+
+static void	compute_ray_length(t_dda *dda, t_pointF ply_pos
+		, t_pointF ray_dir, double *out_dist)
+{
+	double	dist;
+
+	if (dda->side == 0)
+		dist = (dda->map.x - ply_pos.x / MAP_SIZE
+				+ (1 - dda->step.x) / 2) / ray_dir.x;
+	else
+		dist = (dda->map.y - ply_pos.y / MAP_SIZE
+				+ (1 - dda->step.y) / 2) / ray_dir.y;
+	*out_dist = dist * MAP_SIZE;
+	dda->hit_point.x = ply_pos.x + ray_dir.x * (*out_dist);
+	dda->hit_point.y = ply_pos.y + ray_dir.y * (*out_dist);
+}
+
+t_pointF	dda_algo(t_cub *cub, t_pointF ply_pos
+			, t_pointF ray_dir, double *out_dist)
+{
+	t_dda	dda;
+
+	setup_dda(&dda, ply_pos, ray_dir);
+	while (!dda.hit && dda.max_steps-- > 0)
 	{
-		if (sideDist.x < sideDist.y)
+		if (dda.hypotenuse_dist.x < dda.hypotenuse_dist.y)
 		{
-			sideDist.x += deltaDist.x;
-			map.x += step.x;
-			side = 0;
+			dda.hypotenuse_dist.x += dda.unit.x;
+			dda.map.x += dda.step.x;
+			dda.side = 0;
 		}
 		else
 		{
-			sideDist.y += deltaDist.y;
-			map.y += step.y;
-			side = 1;
+			dda.hypotenuse_dist.y += dda.unit.y;
+			dda.map.y += dda.step.y;
+			dda.side = 1;
 		}
-
-		if (map.x < 0 || map.x >= MAP_WIDTH || map.y < 0 || map.y >= MAP_HEIGHT)
-			break;
-
-		if (cub->map[map.y][map.x] == 1)
-			hit = true;
+		if (dda.map.x < 0 || dda.map.x >= MAP_WIDTH
+			|| dda.map.y < 0 || dda.map.y >= MAP_HEIGHT)
+			break ;
+		if (cub->map[dda.map.y][dda.map.x] == 1)
+			dda.hit = true;
 	}
-
-	double dist;
-	if (side == 0)
-		dist = (map.x - start.x / MAP_SIZE + (1 - step.x) / 2) / rayDirX;
-	else
-		dist = (map.y - start.y / MAP_SIZE + (1 - step.y) / 2) / rayDirY;
-
-	*out_dist = dist * MAP_SIZE; // Convert from map units to pixels
-
-	t_pointF hit_point;
-	hit_point.x = start.x + rayDirX * (*out_dist);
-	hit_point.y = start.y + rayDirY * (*out_dist);
-	return hit_point;
+	compute_ray_length(&dda, ply_pos, ray_dir, out_dist);
+	return (dda.hit_point);
 }
-
